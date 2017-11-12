e573656e8e32a646c18a5165dc6602a5f87ca121
James Gomis <jamesislike@gmail.com>
2017-11-12 19:39:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+//Create a constructor function for each of the following:
+//+ Scooter with `year`, `color`, and `model` properties
+//+ Driver with `name`, `age`, and `experience` properties
+//+ PickupLocation with `address` and `city` properties
+
+
+
+class scooter {
+    constructor(year, color, model){
+}
+
+class driver{
+  constructor(name, age, experience)
+}
+
+class PickupLocation{
+  constructor(address, city)
+}
+
