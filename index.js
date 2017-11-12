c929325f2f5431a4573888c62c9716c88a920a76
James Gomis <jamesislike@gmail.com>
2017-11-12 20:00:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
 function sccoter(year, color, model){
-
+  this.year = year;
+  this.color = color
+  this.model = model
 }
 
-let harley = new scooter(2001,'yello','davidson')
+let harley = new Scooter(2001,'yello','davidson')
 
 function driver(name, age, experience){
-
+  this.name = name
+  this.experience = experience
+  this.age = age
+  
 }
 
 let john = new driver('john', 29, '10 years')
