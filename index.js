3a491425474a3ef4fc6e670eb76b04c9af3c437f
James Gomis <jamesislike@gmail.com>
2017-11-12 19:53:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ function sccoter(year, color, model){
 
 }
 
+let harley = new scooter(2001,'yello','davidson')
+
 function driver(name, age, experience){
 
 }
 
+let john = new driver('john', 29, '10 years')
+
 function PickupLocation(address, city){
 
 }
+
+let NYC = new PickupLocation('100 W 47th St', 'New York')
